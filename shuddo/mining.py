337a68dbc162caf7ad2620fd4f906750d423299e
Md Imam Hossain <emamhd@gmail.com>
2020-05-25 08:14:09 +1000

    Handle out of bound data index for S_get_peak

--- shuddo/mining.py
+++ shuddo/mining.py
@@ -16,20 +16,31 @@ def S_get_peak(_data_list, _cursor=0, _base_line=0):
     cursor = _cursor
     peak_width = 0
 
-    while _data_list[cursor] == _base_line:
-        cursor += 1
-
-    if _data_list[cursor] < _base_line:
-        while _data_list[cursor] < _base_line:
-            data_point.append(_data_list[cursor])
+    try:
+        while _data_list[cursor] == _base_line:
             cursor += 1
-            peak_width += 1
-        peak_value = min(data_point)
-    elif _data_list[cursor] > _base_line:
-        while _data_list[cursor] > _base_line:
-            data_point.append(_data_list[cursor])
-            cursor += 1
-            peak_width += 1
-        peak_value = max(data_point)
+    except IndexError:
+        return (0, -1, -1, -1)
 
-    return (peak_value, _cursor+(peak_width/2), peak_width, cursor)
+    try:
+        if _data_list[cursor] < _base_line:
+            while _data_list[cursor] < _base_line:
+                data_point.append(_data_list[cursor])
+                cursor += 1
+                peak_width += 1
+            peak_value = min(data_point)
+        elif _data_list[cursor] > _base_line:
+            while _data_list[cursor] > _base_line:
+                data_point.append(_data_list[cursor])
+                cursor += 1
+                peak_width += 1
+            peak_value = max(data_point)
+
+    except IndexError:
+        if _data_list[cursor-1] < _base_line:
+            peak_value = min(data_point)
+        elif _data_list[cursor-1] > _base_line:
+            peak_value = max(data_point)
+        return (peak_value, _cursor + (peak_width / 2), peak_width, cursor)
+    finally:
+        return (peak_value, _cursor+(peak_width/2), peak_width, cursor)
