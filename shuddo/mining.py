2a09c53f2d4a3c4f22d188b0b72cd0480dd1ab8d
Md Imam Hossain <emamhd@gmail.com>
2020-05-25 07:23:15 +1000

    Add files via upload

---
+++ shuddo/mining.py
@@ -0,0 +1,35 @@
+# Copyright (c) 2020, Md Imam Hossain (emamhd at gmail dot com)
+# see LICENSE.txt for details
+
+"""
+Data mining functions
+"""
+
+def S_get_peak(_data_list, _cursor=0, _base_line=0):
+    """
+    Returns the features of a first positive or negative peak from the data, define horizontal separation for the peak detection by setting the base line, define detection starting location by setting the cursor
+    """
+
+    data_point = []
+    peak_value = 0
+    peak_location = 0
+    cursor = _cursor
+    peak_width = 0
+
+    while _data_list[cursor] == _base_line:
+        cursor += 1
+
+    if _data_list[cursor] < _base_line:
+        while _data_list[cursor] < _base_line:
+            data_point.append(_data_list[cursor])
+            cursor += 1
+            peak_width += 1
+        peak_value = min(data_point)
+    elif _data_list[cursor] > _base_line:
+        while _data_list[cursor] > _base_line:
+            data_point.append(_data_list[cursor])
+            cursor += 1
+            peak_width += 1
+        peak_value = max(data_point)
+
+    return (peak_value, _cursor+(peak_width/2), peak_width, cursor)
